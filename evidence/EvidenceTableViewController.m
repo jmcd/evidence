8be4cb63235087df0ba8faee779ed66d34df3d82
John McDowall <john@stormid.com>
2014-09-25 15:52:00 +0100

    Fixes table view layout on iOS 8

--- evidence/EvidenceTableViewController.m
+++ evidence/EvidenceTableViewController.m
@@ -23,253 +23,244 @@ static NSString *EvidenceTableViewControllerCellReuseIdentifier = @"EvidenceTabl
 @end
 
 @implementation EvidenceTableViewController {
-    UITableView *_tableView;
-    NSFetchedResultsController *_fetchedResultsController;
-    NSString *_addedEvidenceType;
-    ActionSheetController *_actionSheetController;
-    AlertViewController *_deleteAllAlertViewController;
-    CalendarDate *_today;
-    AlertViewController *_alertViewController;
-    UIAlertView *_alertView;
-    ConventionalDateFormatter *_conventionalDateFormatter;
-    FetchedResultsControllerTableViewDelegate *_fetchedResultsControllerTableViewDelegate;
+	UITableView *_tableView;
+	NSFetchedResultsController *_fetchedResultsController;
+	NSString *_addedEvidenceType;
+	ActionSheetController *_actionSheetController;
+	AlertViewController *_deleteAllAlertViewController;
+	CalendarDate *_today;
+	AlertViewController *_alertViewController;
+	UIAlertView *_alertView;
+	ConventionalDateFormatter *_conventionalDateFormatter;
+	FetchedResultsControllerTableViewDelegate *_fetchedResultsControllerTableViewDelegate;
 }
 
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
-    Evidence *evidence = [_fetchedResultsController objectAtIndexPath:indexPath];
-    NSString *string = [_conventionalDateFormatter timeStringFromDate:evidence.createdOnDateTime];
-    cell.detailTextLabel.text = string;
-    cell.textLabel.text = evidence.type;
-    cell.imageView.image = [UIImage imageWithData:evidence.thumbnailImageData];
+	Evidence *evidence = [_fetchedResultsController objectAtIndexPath:indexPath];
+	NSString *string = [_conventionalDateFormatter timeStringFromDate:evidence.createdOnDateTime];
+	cell.detailTextLabel.text = string;
+	cell.textLabel.text = evidence.type;
+	cell.imageView.image = [UIImage imageWithData:evidence.thumbnailImageData];
 }
 
 - (NSFetchRequest *)constructFetchRequest {
-    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[Evidence entityName]];
-    [fetchRequest setSortDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"createdOnDateTime" ascending:NO]]];
-    return fetchRequest;
+	NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[Evidence entityName]];
+	[fetchRequest setSortDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"createdOnDateTime" ascending:NO]]];
+	return fetchRequest;
 }
 
 - (void)beginAdd {
 
-    _alertViewController = [[AlertViewController alloc] initWithTitle:@"What are you collecting evidence of?" message:nil cancelAction:[Action actionWithTitle:@"Cancel"] otherActions:@[[Action actionWithTitle:@"OK" block:^{
-        UITextField *textField = [_alertViewController.alertView textFieldAtIndex:0];
-        _addedEvidenceType = textField.text;
-        [self presentImagePickerController];
-    }]]];
+	_alertViewController = [[AlertViewController alloc] initWithTitle:@"What are you collecting evidence of?" message:nil cancelAction:[Action actionWithTitle:@"Cancel"] otherActions:@[[Action actionWithTitle:@"OK" block:^{
+		UITextField *textField = [_alertViewController.alertView textFieldAtIndex:0];
+		_addedEvidenceType = textField.text;
+		[self presentImagePickerController];
+	}]]];
 
-    NSMutableArray *actions = [NSMutableArray array];
+	NSMutableArray *actions = [NSMutableArray array];
 
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 
-    for (int i = 0; i < 6; i++) {
-        NSString *key = [NSString stringWithFormat:@"type_%d_preference", i];
-        NSString *string = [defaults objectForKey:key];
-        string = [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-        if (string.length > 0) {
-            Action *action = [Action actionWithTitle:string block:^() {
-                _addedEvidenceType = string;
-                [self presentImagePickerController];
-            }];
-            [actions addObject:action];
-        }
-    }
+	for (int i = 0; i < 6; i++) {
+		NSString *key = [NSString stringWithFormat:@"type_%d_preference", i];
+		NSString *string = [defaults objectForKey:key];
+		string = [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+		if (string.length > 0) {
+			Action *action = [Action actionWithTitle:string block:^() {
+				_addedEvidenceType = string;
+				[self presentImagePickerController];
+			}];
+			[actions addObject:action];
+		}
+	}
 
-    Action *somethingElse = [Action actionWithTitle:@"Something else..." block:^() {
-        _alertView = _alertViewController.alertView;
-        _alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
-        [_alertView show];
-    }];
+	Action *somethingElse = [Action actionWithTitle:@"Something else..." block:^() {
+		_alertView = _alertViewController.alertView;
+		_alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+		[_alertView show];
+	}];
 
-    [actions addObject:somethingElse];
+	[actions addObject:somethingElse];
 
-    Action *cancelAction = [Action actionWithTitle:@"Cancel" block:^() {
-        _addedEvidenceType = nil;
-    }];
+	Action *cancelAction = [Action actionWithTitle:@"Cancel" block:^() {
+		_addedEvidenceType = nil;
+	}];
 
-    _actionSheetController = [[ActionSheetController alloc] initWithTitle:@"What are you collecting evidence of?\n(Customize these in the Settings app)" cancelAction:cancelAction destructiveAction:nil otherActions:actions];
+	_actionSheetController = [[ActionSheetController alloc] initWithTitle:@"What are you collecting evidence of?\n(Customize these in the Settings app)" cancelAction:cancelAction destructiveAction:nil otherActions:actions];
 
-    [_actionSheetController.actionSheet showInView:self.view];
+	[_actionSheetController.actionSheet showInView:self.view];
 }
 
 - (void)beginDeleteAll {
 
-    _deleteAllAlertViewController = [[AlertViewController alloc] initWithTitle:@"Delete all evidence?" message:nil cancelAction:[Action actionWithTitle:@"Keep"] otherActions:@[
-        [Action actionWithTitle:@"Delete all" block:^() {
+	_deleteAllAlertViewController = [[AlertViewController alloc] initWithTitle:@"Delete all evidence?" message:nil cancelAction:[Action actionWithTitle:@"Keep"] otherActions:@[
+		[Action actionWithTitle:@"Delete all" block:^() {
 
-            NSArray *allEvidences = [[CoreDataHelper instance] executeFetchRequestOnMainQueueContext:[NSFetchRequest fetchRequestWithEntityName:[Evidence entityName]]];
+			NSArray *allEvidences = [[CoreDataHelper instance] executeFetchRequestOnMainQueueContext:[NSFetchRequest fetchRequestWithEntityName:[Evidence entityName]]];
 
-            for (id evidence in allEvidences) {
-                [[CoreDataHelper instance].mainQueueContext deleteObject:evidence];
-            }
+			for (id evidence in allEvidences) {
+				[[CoreDataHelper instance].mainQueueContext deleteObject:evidence];
+			}
 
-            [[CoreDataHelper instance] saveMainQueueContext];
-        }]
-    ]];
+			[[CoreDataHelper instance] saveMainQueueContext];
+		}]
+	]];
 
-    [_deleteAllAlertViewController.alertView show];
+	[_deleteAllAlertViewController.alertView show];
 }
 
 - (void)presentImagePickerController {
 
-    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
-    imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
-    imagePickerController.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
-    imagePickerController.delegate = self;
+	UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+	imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+	imagePickerController.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+	imagePickerController.delegate = self;
 
-    [self presentViewController:imagePickerController animated:YES completion:nil];
+	[self presentViewController:imagePickerController animated:YES completion:nil];
 }
 
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+	[super viewDidLoad];
 
-    _conventionalDateFormatter = [[ConventionalDateFormatter alloc] init];
+	_conventionalDateFormatter = [[ConventionalDateFormatter alloc] init];
 
-    self.title = @"Evidence";
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(beginAdd)];
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(beginDeleteAll)];
+	self.title = @"Evidence";
+	self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(beginAdd)];
+	self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(beginDeleteAll)];
 
-    _tableView = (UITableView *) [self.view addConstrainedSubview:[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped]];
-    [_tableView registerClass:[EvidenceTableViewCell class] forCellReuseIdentifier:EvidenceTableViewControllerCellReuseIdentifier];
-    _tableView.dataSource = self;
-    _tableView.delegate = self;
+	_tableView = (UITableView *) [self.view addConstrainedSubview:[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped]];
+	[_tableView registerClass:[EvidenceTableViewCell class] forCellReuseIdentifier:EvidenceTableViewControllerCellReuseIdentifier];
+	_tableView.dataSource = self;
+	_tableView.delegate = self;
 
-    [self.view setNeedsUpdateConstraints];
+	NSDictionary *views = NSDictionaryOfVariableBindings(_tableView);
 
-    _today = [CalendarDate today];
+	[self.view addManyConstraints:@[
 
-    NSManagedObjectContext *context = [CoreDataHelper instance].mainQueueContext;
+		[NSLayoutConstraint constraintsWithVisualFormats:@[
+			@"V:|[_tableView]|",
+			@"H:|[_tableView]|",
+		] options:(NSLayoutFormatOptions) 0 metrics:nil views:views],
+	]];
 
-    _fetchedResultsControllerTableViewDelegate = [[FetchedResultsControllerTableViewDelegate alloc] initWithDelegate:self];
+	_today = [CalendarDate today];
 
-    NSFetchRequest *fetchRequest = [self constructFetchRequest];
-    _fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:context sectionNameKeyPath:@"createdOnDate" cacheName:nil];
-    _fetchedResultsController.delegate = _fetchedResultsControllerTableViewDelegate;
+	NSManagedObjectContext *context = [CoreDataHelper instance].mainQueueContext;
 
-    NSError *error;
-    ZAssert([_fetchedResultsController performFetch:&error], @"error performing fetch: %@, %@", error.localizedDescription, error.userInfo);
+	_fetchedResultsControllerTableViewDelegate = [[FetchedResultsControllerTableViewDelegate alloc] initWithDelegate:self];
 
-    [NSTimer scheduledTimerWithTimeInterval:10 target:self selector:@selector(checkTodayIsValid) userInfo:nil repeats:YES];
+	NSFetchRequest *fetchRequest = [self constructFetchRequest];
+	_fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:context sectionNameKeyPath:@"createdOnDate" cacheName:nil];
+	_fetchedResultsController.delegate = _fetchedResultsControllerTableViewDelegate;
+
+	NSError *error;
+	ZAssert([_fetchedResultsController performFetch:&error], @"error performing fetch: %@, %@", error.localizedDescription, error.userInfo);
+
+	[NSTimer scheduledTimerWithTimeInterval:10 target:self selector:@selector(checkTodayIsValid) userInfo:nil repeats:YES];
 }
 
 - (void)checkTodayIsValid {
-    CalendarDate *today = [CalendarDate today];
-    if ([today isAfter:_today]){
-        _today = today;
-        [_tableView reloadData];
-    }
+	CalendarDate *today = [CalendarDate today];
+	if ([today isAfter:_today]) {
+		_today = today;
+		[_tableView reloadData];
+	}
 }
-
-- (void)updateViewConstraints {
-    [super updateViewConstraints];
-
-    NSDictionary *views = NSDictionaryOfVariableBindings(_tableView);
-
-    [self.view removeConstraints:self.view.constraints];
-
-    [self.view addManyConstraints:@[
-
-        [NSLayoutConstraint constraintsWithVisualFormats:@[
-            @"V:|[_tableView]|",
-            @"H:|[_tableView]|",
-        ] options:(NSLayoutFormatOptions) 0 metrics:nil views:views],
-    ]];
-}
-
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return [[_fetchedResultsController sections] count];
+	return [[_fetchedResultsController sections] count];
 }
 
 - (NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section {
-    if ([[_fetchedResultsController sections] count] > 0) {
-        id <NSFetchedResultsSectionInfo> sectionInfo = [[_fetchedResultsController sections] objectAtIndex:section];
-        return [sectionInfo numberOfObjects];
-    } else
-        return 0;
+	if ([[_fetchedResultsController sections] count] > 0) {
+		id <NSFetchedResultsSectionInfo> sectionInfo = [[_fetchedResultsController sections] objectAtIndex:section];
+		return [sectionInfo numberOfObjects];
+	} else
+		return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:EvidenceTableViewControllerCellReuseIdentifier forIndexPath:indexPath];
-    [self configureCell:cell atIndexPath:indexPath];
-    return cell;
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:EvidenceTableViewControllerCellReuseIdentifier forIndexPath:indexPath];
+	[self configureCell:cell atIndexPath:indexPath];
+	return cell;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    if ([[_fetchedResultsController sections] count] > 0) {
-        id <NSFetchedResultsSectionInfo> sectionInfo = [[_fetchedResultsController sections] objectAtIndex:section];
+	if ([[_fetchedResultsController sections] count] > 0) {
+		id <NSFetchedResultsSectionInfo> sectionInfo = [[_fetchedResultsController sections] objectAtIndex:section];
 
-        Evidence *evidence = [sectionInfo.objects firstObject];
-        NSDate *date = evidence.createdOnDate;
-        return [_conventionalDateFormatter dayDescriptionStringFromDate:date releativeToToday:_today.date];
-    } else {
-        return nil;
-    }
+		Evidence *evidence = [sectionInfo.objects firstObject];
+		NSDate *date = evidence.createdOnDate;
+		return [_conventionalDateFormatter dayDescriptionStringFromDate:date releativeToToday:_today.date];
+	} else {
+		return nil;
+	}
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
+	return YES;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [[CoreDataHelper instance].mainQueueContext deleteObject:[_fetchedResultsController objectAtIndexPath:indexPath]];
-        [[CoreDataHelper instance] saveMainQueueContext];
-    }
+	if (editingStyle == UITableViewCellEditingStyleDelete) {
+		[[CoreDataHelper instance].mainQueueContext deleteObject:[_fetchedResultsController objectAtIndexPath:indexPath]];
+		[[CoreDataHelper instance] saveMainQueueContext];
+	}
 }
 
 #pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
 
-    UIViewController *controller;
+	UIViewController *controller;
 
-    Evidence *evidence = [_fetchedResultsController objectAtIndexPath:indexPath];
-    if ([evidence.mediaType isEqualToString:(NSString *) kUTTypeMovie]) {
-        controller = [[VideoViewController alloc] initWithDataFilePath:evidence.dataFilePath];
-    }
+	Evidence *evidence = [_fetchedResultsController objectAtIndexPath:indexPath];
+	if ([evidence.mediaType isEqualToString:(NSString *) kUTTypeMovie]) {
+		controller = [[VideoViewController alloc] initWithDataFilePath:evidence.dataFilePath];
+	}
 
-    if ([evidence.mediaType isEqualToString:(NSString *) kUTTypeImage]) {
-        UIImage *image = [UIImage imageWithData:evidence.data];
-        controller = [[ImageViewController alloc] initWithImage:image];
-    }
+	if ([evidence.mediaType isEqualToString:(NSString *) kUTTypeImage]) {
+		UIImage *image = [UIImage imageWithData:evidence.data];
+		controller = [[ImageViewController alloc] initWithImage:image];
+	}
 
-    controller.title = [NSString stringWithFormat:@"%@, %@", evidence.type, [_conventionalDateFormatter longStringFromDate:evidence.createdOnDateTime]];
-    [self.navigationController pushViewController:controller animated:YES];
+	controller.title = [NSString stringWithFormat:@"%@, %@", evidence.type, [_conventionalDateFormatter longStringFromDate:evidence.createdOnDateTime]];
+	[self.navigationController pushViewController:controller animated:YES];
 }
 
 #pragma mark - FetchedResultsControllerTableViewDelegateDelegate
 
 - (UITableView *)tableViewForFetchedResultsControllerTableViewDelegate:(FetchedResultsControllerTableViewDelegate *)fetchedResultsControllerTableViewDelegate {
-    return _tableView;
+	return _tableView;
 }
 
 - (void)fetchedResultsControllerTableViewDelegate:(FetchedResultsControllerTableViewDelegate *)fetchedResultsControllerTableViewDelegate configureCellAtIndexPath:(NSIndexPath *)indexPath {
-    [self configureCell:[_tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+	[self configureCell:[_tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
 }
 
 #pragma mark - UIImagePickerControllerDelegate
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
 
-    NSString *mediaType = info[UIImagePickerControllerMediaType];
-    NSURL *mediaUrl = info[UIImagePickerControllerMediaURL];
-    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
-    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+	NSString *mediaType = info[UIImagePickerControllerMediaType];
+	NSURL *mediaUrl = info[UIImagePickerControllerMediaURL];
+	UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+	UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
 
-    Evidence *evidence = [EvidenceFactory constructEvidenceWithMediaType:mediaType mediaUrl:mediaUrl editedImage:editedImage originalImage:originalImage evidenceType:_addedEvidenceType];
+	Evidence *evidence = [EvidenceFactory constructEvidenceWithMediaType:mediaType mediaUrl:mediaUrl editedImage:editedImage originalImage:originalImage evidenceType:_addedEvidenceType];
 
-    [Notifications scheduleNotificationForEvidence:evidence];
+	[Notifications scheduleNotificationForEvidence:evidence];
 
-    [self dismissViewControllerAnimated:YES completion:nil];
+	[self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-    [self dismissViewControllerAnimated:YES completion:nil];
+	[self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
